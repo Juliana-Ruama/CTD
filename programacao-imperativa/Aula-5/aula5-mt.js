bf6f0e986c7029a47234129bcdf21033a57079f2
unknown <ruamajuliana@gmail.com>
2021-10-26 23:31:21 -0300

    Aula de introdução ao CSS

---
+++ programacao-imperativa/Aula-5/aula5-mt.js
@@ -0,0 +1,61 @@
+// 1) 30
+
+// 2) 20
+
+// 3) 
+
+// 1) 30
+
+// 2) 20
+
+// 3)a)
+//function polegada_cm(x){
+   //return x * 2.54
+} //
+
+//b)
+function URL(x){
+   return "http://www./%22+x+%22.com.br"
+} //
+
+// c)
+function exclamacao(x){
+    return x+"!"
+} //
+
+// d) 
+function human-to-dog(x) {
+    return x * 7
+} //
+
+// e)
+function salario(x){
+    return x / 160
+}// 
+
+// f) 
+function IMC(peso, altura) {
+    return peso / ((altura * altura)/100)
+} //
+//console.log(80, 170)
+
+//acho que não rolou aqui não
+
+//g)
+
+//function a (x,y){
+    console.log(x)
+    return x * 2
+    console.log(x)
+    return x / 2
+}// 
+
+//console.log(a(10) );
+
+//function polegada_cm(x){
+    //return x * 2.54
+} //
+
+//console.log (polegada_cm (25) );
+
+
---
+++ programacao-imperativa/Aula-5/aula5.js
@@ -0,0 +1,28 @@
+function pessoa(nome, idade){
+    //criar função return nome+ "tem "+idade;//retorna calculo
+}
+
+
+function soma (x,y){
+    return x + y ;
+}
+
+console.log(soma (15, 10) ); 
+
+function pessoa(nome, idade){
+    return nome+"tem"+idade
+}
+
+console.log (pessoa(João, 25)); 
+
+function somar(x,y){
+    consult.log ("aqui é a função");
+    return x+y;
+}
+
+let result = somar (25,5);
+console.log( somar (20,15) );
+console.log( somar(8,15) );
+console.log(somar (90,15) );
+
+console.log("Esse é o resultado : " +result);
